13b97237315ddaf1094f2aa175cc5c1b091befda
Rachel Perkins <rachelperkins@Rachels-MBP.lan>
2021-03-21 09:49:40 -0700

    Stack class and start building stacks

---
+++ script.py
@@ -0,0 +1,19 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+
+
+#Set up the Game
+
+
+#Get User Input
+
+        
+#Play the Game
